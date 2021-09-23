8907cb64987ae95d56c516740c9e73124e708739
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-23 19:50:31 +0800

    recreate a list view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
+from django.shortcuts import render
+from django.views.generic.list import MultipleObjectMixin
+
 from .models import Product, DigitalProduct
 from .mixins import TemplateTitleMixin
 
@@ -11,9 +14,56 @@ class DigitalProductListView(TemplateTitleMixin, ListView):
         return 'My New Title'
 
 
-class ProductListView(TemplateTitleMixin, ListView):
+class ProductOGListView(TemplateTitleMixin, ListView):
     model = Product
     title = 'Products'
+
+
+class QuerysetModelMixin():
+    '''
+    Ref for MultipleObjectMixin
+    '''
+    model = None
+    queryset = None
+
+    def get_template(self):
+        print('self.queryset.model: ', self.queryset.model)
+        if self.model is None:
+            raise Exception('You need to set a model')
+        model_name = self.model._meta.model_name
+        app_label = self.model._meta.app_label
+        template = f'{app_label}/{model_name}_list.html'
+        return template
+
+    def get_queryset(self):
+        qs = None
+        if self.queryset is not None:
+            qs = self.queryset
+        elif self.model is not None:
+            qs = self.model.objects.all()
+        else:
+            raise Exception('You need to set a model or a queryset')
+        self.model = qs.model
+        return qs
+
+    def get_context_data(self):
+        context = {
+            'object_list': self.get_queryset()
+        }
+        return context
+
+
+# class ProductListView(QuerysetModelMixin, View):
+class ProductListView(MultipleObjectMixin, View):
+    queryset = Product.objects.filter(pk__gte=1)
+
+    def get(self, req, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+        app_label = self.object_list.model._meta.app_label
+        model_name = self.object_list.model._meta.model_name
+        template = f'{app_label}/{model_name}_list.html'
+        return render(req, template, context)
 
 
 class ProductDetailView(TemplateTitleMixin, DetailView):
